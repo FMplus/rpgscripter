fbd9edecec43be04b2ed4306ccc7758a194e607c
lsmcsp@gmail.com <lsmcsp@gmail.com>
2014-09-05 23:00:25 +0800

    Create the syntax file and basical file

---
+++ src/tag.h
@@ -0,0 +1,23 @@
+#ifndef _TAG_H
+#define _TAG_H
+
+namespace lexer {
+    typedef int tag_t;
+    namespace tag{
+        const tag_t AND   = 256,BASIC = 257,BREAK = 258,
+                    DO    = 259,ELSE  = 260,EQ    = 261,
+                    FALSE = 262,GE    = 263,ID    = 264,
+                    AND   = 256,BASIC = 257,BREAK = 258,
+                    DO    = 259,ELSE  = 260,EQ    = 261,
+                    FALSE = 262,GE    = 263,ID    = 264,
+                    IF    = 265,INDEX = 266,LE    = 267,
+                    MINUS = 268,NE    = 269,NUM   = 270,
+                    OR    = 271,REAL  = 272,
+                    TRUE  = 274,WHILE = 275,INC   = 276,
+                    DEC   = 277,FOR   = 278,STR   = 279,
+                    RETURN = 282,STAGE = 283,VAR = 284,
+
+    };//namespace tag
+};//namespace lexer
+
+#endif//_TAG_H
