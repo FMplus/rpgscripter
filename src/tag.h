30007a2de80f9b982f4eff7eec02765adea5f04c
lsmcsp@gmail.com <lsmcsp@gmail.com>
2014-09-07 11:25:38 +0800

    Fixed some mistakes

--- src/tag.h
+++ src/tag.h
@@ -7,15 +7,15 @@ namespace lexer {
         const tag_t AND   = 256,BASIC = 257,BREAK = 258,
                     DO    = 259,ELSE  = 260,EQ    = 261,
                     FALSE = 262,GE    = 263,ID    = 264,
-                    AND   = 256,BASIC = 257,BREAK = 258,
-                    DO    = 259,ELSE  = 260,EQ    = 261,
-                    FALSE = 262,GE    = 263,ID    = 264,
                     IF    = 265,INDEX = 266,LE    = 267,
                     MINUS = 268,NE    = 269,NUM   = 270,
                     OR    = 271,REAL  = 272,
                     TRUE  = 274,WHILE = 275,INC   = 276,
                     DEC   = 277,FOR   = 278,STR   = 279,
-                    RETURN = 282,STAGE = 283,VAR = 284,
+                    RETURN = 282;
+        const tag_t STAGE = 283,VAR = 284,MAP   = 285,
+                    GLOBAL = 286,LOCAL = 287;
+                    
 
     };//namespace tag
 };//namespace lexer
